1804700b622527251b633a785d07e588973299fb
Pjotr Prins <pjotr.public01@thebird.nl>
2017-09-11 14:34:18 +0000

    Added template

--- yaj/views.py
+++ yaj/views.py
@@ -1,5 +1,11 @@
 from yaj import app
+from flask import render_template
+
+@app.route("/test")
+def test_page():
+    return "Yet another journal! Well, early days!"
 
 @app.route("/")
 def main_page():
-    return "Yet another journal! Well, early days!"
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template("index.html",title="YAJ",user=user)
