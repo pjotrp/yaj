66ee9a08255c2e9083f2eb6d0597fb4f2047180c
Pjotr Prins <pjotr.public01@thebird.nl>
2017-09-14 06:38:33 +0000

    A few fixes

--- yaj/publish.py
+++ yaj/publish.py
@@ -4,11 +4,11 @@ from pygit2 import Repository
 
 from yaj.config import YAJ_DIR, YAJ_GIT
 import yaj.kjson as kjson
+import yaj.uri as uri
+import yaj.fetch as fetch
 
 def story_metadata(id):
     return kjson.loadf(YAJ_DIR+"/doc/"+id+".jsonld")
-
-from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE, GIT_SORT_TIME
 
 def story(content_uri):
     """Return the text referenced by content_uri. If uri can not be
@@ -16,21 +16,8 @@ def story(content_uri):
     netloc 'yaj-dir' resolves to the local source tree.
 
     """
-    o = urlparse(content_uri)
-    if o.scheme == 'git' and o.netloc == 'yaj-dir':
-        fullpath = YAJ_DIR+'/'+o.path
-        repo = Repository(YAJ_GIT)
-        index = repo.index
-        index.read()
-        id = index['yaj'+o.path].id    # from path to object id
-        print(id)
-        # for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL|GIT_SORT_TIME):
-        #     print(commit.commit_time, ' ', commit.message)
-        f = open(fullpath)
-        buf = f.read()
-        f.close
-    else:
-        raise Exception("Can not parse URI "+content_uri)
+    fullpath = uri.resolve_to_path(content_uri)
+    buf = fetch.read(fullpath)
     rndr = HtmlRenderer()
     md = Markdown(rndr)
     return md(buf)
--- yaj/uri.py
+++ yaj/uri.py
@@ -1,12 +1,16 @@
 import os
 from urllib.parse import urlparse
 
+from yaj.config import YAJ_DIR, YAJ_GIT
+
 def resolve_to_path(uri):
     """Turn a URI into a file name, if possible. On failure return uri as is
     """
     o = urlparse(uri)
     print(o)
     path = o.path
+    if o.scheme == 'git' and o.netloc == 'yaj-dir':
+        path = YAJ_DIR+'/'+o.path
     if o.netloc == '{{HOME}}':
         path = os.environ.get('HOME')+path
     if not os.path.exists(path):
