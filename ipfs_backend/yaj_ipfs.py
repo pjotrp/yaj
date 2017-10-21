66c1ee711f2cd38a0121557f4ece1f722f019f41
Francesco Strozzi <francesco.strozzi@gmail.com>
2017-10-21 14:40:20 +0200

    added other methods

--- ipfs_backend/yaj_ipfs.py
+++ ipfs_backend/yaj_ipfs.py
@@ -10,11 +10,36 @@ api = ipfsapi.connect('127.0.0.1', 5001)
 Accepts an object name (for now a file on the local path) and store it on IPFS
 Returns a dict with the object name and the IPFS hash
 """
-@app.route("/store/put/<obj>", methods=['POST'])
+@app.route("/store/put/<obj>", methods=['PUT'])
 def put(obj):
-    if request.methd == 'POST':
+    if request.method == 'PUT':
         result = api.add(obj)
-        return result
+        print(result)
+        return "Put request processed: {0}".format(result)
+
+
+"""
+Lists object by IPFS hash, returns a dict
+"""
+@app.route("/store/list/<obj_hash>",methods=['GET'])
+def list(obj_hash):
+    if request.method == 'GET':
+        result = api.ls(obj_hash)
+        print(result)
+        return "Get request processed: {0}".format(result)
+
+
+"""
+Accepts an object IPFS hash and download a local copy of that object
+"""
+@app.route("/store/get/<obj_hash>",methods=['GET'])
+def get(obj_hash):
+    if request.method == 'GET':
+        result = api.get(obj_hash)
+        print(result)
+        return "Get request processed: {0}".format(result)
+
+### Requires generating keys using key_gen(). UNTESTED
 
 """
 Receives a name (either a local IPFS hash or a IPNS path) and resolve it to
@@ -23,9 +48,8 @@ an IPFS hash
 @app.route("/store/resolve/<name>",methods=['GET'])
 def resolve(name):
     if request.method == 'GET':
-        path = api.resolve(name)
-        return path
-
+        path = api.resolve("/ipfs/{0}".format(name))
+        return "Resolve request processed: {0}".format(path)
 
 """
 Takes an IPFS path of an object and publish it on IPNS.
@@ -35,7 +59,9 @@ Returns a dict with the IPNS hash and the IPFS path pointing to it
 def publish(obj_path):
     if request.method == 'POST':
         result = api.name_publish(obj_path)
-        return result
+        return "Publish request processed: {0}".format(result)
 
 
 
+if __name__ == "__main__":
+    app.run()
